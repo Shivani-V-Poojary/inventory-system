50dc8132225f79af6b7f26f4d32efa8b380bace3
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 15:59:07 +0530

    Update register.php

--- register.php
+++ register.php
@@ -3,6 +3,7 @@
    $email = $_REQUEST['email'];
    $phone = $_REQUEST['phone'];
    echo "<h1>".$name.$email.$phone."</h1>"; 
-   $str_output = shell_exec('python register.py $name $email $phone');
+   $command = system("python register.py $name $email $phone > /dev/null 2>&1 &");
+   $str_output = shell_exec($command);
    echo $str_output;
 ?>
