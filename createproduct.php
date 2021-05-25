a9530bca2328566471b91d121a3e4ac0638eac87
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 16:27:00 +0530

    Update createproduct.php

--- createproduct.php
+++ createproduct.php
@@ -1,5 +1,9 @@
 <?php
-   $command_exec = escapeshellcmd('python createproduct.py');
+   $pid = $_REQUEST['image'];
+   $pname = $_REQUEST['name'];
+   $quan = $_REQUEST['quantity'];
+   $price = $_REQUEST['price'];
+   $command_exec = escapeshellcmd('python createproduct.py'.' '.$name.' '.$pid.' '.$quan.' '.$price);
    $str_output = shell_exec($command_exec);
    echo $str_output;
 ?>
