906ae9e69b0ebeb9f73881a80a7185458a7a32f5
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 15:00:06 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,10 @@
 <?php
+if ( isset( $_POST['submit'] ) ) {
+
+   $name = $_REQUEST['name'];
+   $email = $_REQUEST['email'];
+   $phone = $_REQUEST['phone'];
+   system("python register.py $name $email $phone > /dev/null 2>&1 &");
    $command_exec = escapeshellcmd('python register.cgi');
    $str_output = shell_exec($command_exec);
    echo $str_output;
