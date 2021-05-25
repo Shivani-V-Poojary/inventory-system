dacc663564a90a8c8c30ed1a9d4bd60d5bd8ddb9
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 15:20:42 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,8 @@
 <?php
-if ( isset( $_REQUEST['reg_user'] ) ) {
 
    $name = $_REQUEST['name'];
    $email = $_REQUEST['email'];
    $phone = $_REQUEST['phone'];
    system("python register.py $name $email $phone > /dev/null 2>&1 &");
-}
+
 ?>
