4dda78bbbad665967b94d3d390084b6242f38d64
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 18:19:50 +0530

    Update orderplace.php

--- orderplace.php
+++ orderplace.php
@@ -1,12 +1,10 @@
 <?php
-   $cname = $_GET['clientname'];
-   $num = $_GET['number'];
    $place = $_REQUEST['place'];
    $name = $_REQUEST['pname'];
    $qua = $_REQUEST['pquantity'];
    $price = $_REQUEST['pprice'];
    echo $cname.$num.$place.$name.$qua.$price
-   /*$command_exec = escapeshellcmd('python orderplace.py'. ".$cname." .$num. '.$place.' ".$name." .$qua. .$price);
+   /*$command_exec = escapeshellcmd('python orderplace.py'. '.$place.' ".$name." .$qua. .$price);
    $str_output = shell_exec($command_exec);
    echo $str_output;*/
 ?>
