c0ad4e770f197e0a6186c84df2abbe02faf93069
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 13:18:53 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-   $command_exec = escapeshellcmd('python /cgi-bin/register.py');
+   $command_exec = escapeshellcmd('python register.py');
    $str_output = shell_exec($command_exec);
    echo $str_output;
 ?>
