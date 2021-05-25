5ac5a85a0f566f801e0b5d58a9b9ec0e9c44a559
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 16:12:11 +0530

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,6 @@
    $name = $_REQUEST['name'];
    $email = $_REQUEST['email'];
    $phone = $_REQUEST['phone'];
-   echo "<h1>".$name.$email.$phone."</h1>";
    $command = escapeshellcmd('python register.py'.' '.$name.' '.$email.' '.$phone);
    $str_output = shell_exec($command);
    echo $str_output;
