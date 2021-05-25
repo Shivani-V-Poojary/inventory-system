87895ddda6ce0c0f00875aaad23b17b81ef91388
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 15:33:44 +0530

    Update register.php

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
    $name = $_REQUEST['name'];
    $email = $_REQUEST['email'];
    $phone = $_REQUEST['phone'];
+   echo "<h1><center> Login successful </center></h1>"; 
    $str_output = shell_exec('python register.py . $name $email $phone');
    echo $str_output;
 ?>
