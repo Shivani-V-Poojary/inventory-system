5edc17549fea26f93d7384f3be8c9cd0af45106f
Shivani-V-Poojary <71928764+Shivani-V-Poojary@users.noreply.github.com>
2021-05-25 16:03:57 +0530

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
    $email = $_REQUEST['email'];
    $phone = $_REQUEST['phone'];
    echo "<h1>".$name.$email.$phone."</h1>";
-   $command = escapeshellcmd('python admindisplay.py $name $email $phone');
+   $command = escapeshellcmd('python register.py $name $email $phone');
    $str_output = shell_exec($command);
    echo $str_output;
 ?>
